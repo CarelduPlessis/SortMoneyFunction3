c7dc7ee2c6acd86604e68be6e8510085264ce265
cdup <cdup@MSI>
2020-07-17 09:51:22 +1200

    Add project files.

---
+++ Readme.dm
@@ -0,0 +1,38 @@
+************************************************************************************
+************************************************************************************
+
+	Goal of this program
+
+************************************************************************************
+
+************************************************************************************
+
+* Sort money in to groups of smaller bills and count the groups, for example 258 will be sorted like this:
+	
+	100: 2   50: 1  20: 0  10: 0  5: 1   2: 1    1: 1
+
+* Learn about "IF Statment" by using them to sort money in different textboxes.
+
+* Learn how use functions.
+
+* Learning about the "out" key word and how to use it in a function to return two values
+
+* Continue trying out new ideas on how to improve my code.
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ SortMoneyFunction3/SortMoney.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SortMoneyFunction3
+{
+    public partial class SortMoney : Form
+    {
+        public SortMoney()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSortMoney_Click(object sender, EventArgs e)
+        {
+            // Declaring variable 
+            int[] size = { 100, 50, 20, 10, 5, 2, 1 };
+            TextBox textBox;
+            int money1 = 0; // Store results
+            int money2 = Convert.ToInt32(tbxEnterValue.Text); // User Input: Total amount of money to be sorted
+
+            for (int i = 0; i < size.Length; i++)
+            { 
+                textBox = (TextBox)this.Controls["tbxMoney" + size[i]];
+                // Pass variables in to the method 
+                // using out keyword 
+                Addition(money2, out money1, size[i]); // call function
+                textBox.Text = money1.ToString(); // Display results in textbox
+                money2 = money2 - (money1 * size[i]); // calculating how much is left of the "User Input" value
+            }
+        }
+
+        // Method in which out parameter is passed 
+        // and this method returns the value of 
+        // the passed parameter 
+        public static void Addition(int money2, out int money1, int size)
+        {
+            money1 = money2 / size;
+        }
+    }
+}
